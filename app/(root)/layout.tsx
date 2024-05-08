e42a81be55c4b0e2af47a1b5ed2e116274821691
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2024-05-08 17:19:01 +0530

    Start Home Page UI partially
    Added Charts & more on ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import { Sidebar } from "lucide-react";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Akanksha", lastName: "Pandey" };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar />
       {children}
     </main>
   );
